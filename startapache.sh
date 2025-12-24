b2157bd95e450a9d6ae42172e660a1c673b918b7
devrazec <devrazec@gmail.com>
2025-12-24 23:14:20 +0100

    Update

--- startapache.sh
+++ startapache.sh
@@ -14,7 +14,5 @@ php artisan migrate --force
 # Start Apache in the background
 apache2-foreground &
 
-# Start Vite in the background (adjust path as needed)
-cd /var/www/html && npm run dev &
 # Wait on all background processes
 wait
