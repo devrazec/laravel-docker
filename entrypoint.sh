68b1a5878d3f77e2095a30294ee04db578025c9e
devrazec <devrazec@gmail.com>
2025-12-24 13:24:52 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-composer install --no-dev --working-dir=/var/www/html
-
 # Optional: Laravel commands before starting Apache
 php artisan config:clear
 php artisan cache:clear
@@ -10,7 +8,6 @@ php artisan view:clear
 php artisan route:clear
 php artisan config:cache
 php artisan route:cache
-php artisan key:generate
 
 touch /var/www/html/database/database.sqlite
 php artisan migrate --force
